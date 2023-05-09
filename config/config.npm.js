0f79a479b8cbb1c11858fc8e591cc97562ed2d3b
fyr <317883224@qq.com>
2023-05-09 15:32:20 +0800

    调整打包逻辑

--- config/config.npm.js
+++ config/config.npm.js
@@ -1,10 +1,10 @@
 /*
  * @FileName: config.npm.js
- * @FilePath: \vue2-vant2-components\config\config.npm.js
+ * @FilePath: \configjs-utilsjs\config\config.npm.js
  * @Author: FYR
  * @Date: 2023-04-12 15:29:48
  * @LastEditors: FYR
- * @LastEditTime: 2023-04-17 10:22:59
+ * @LastEditTime: 2023-05-09 15:30:50
  * @Description: 生成npm配置文件的配置文件
  */
 
@@ -13,10 +13,10 @@ const { resolve } = require('path');
 const writerStream = fs.createWriteStream(resolve('./dist/package.json'));
 let packageData = require('../package.json') || {};
 
-JSON.parse(JSON.stringify(packageData));
 packageData.private = false;
 delete packageData.scripts;
 delete packageData.devDependencies;
 writerStream.write(JSON.stringify(packageData),'utf-8');
 writerStream.end();
 fs.copyFileSync('./NPM.README.md', './dist/README.md');
+fs.renameSync('./dist/main', './dist/lib');
